46f4cbca16c49fbde5e51326c27603102996bc11
uauramenezes <uaurasilva@hotmail.com>
2021-04-27 19:44:48 -0300

    Finish get, post and delete router

--- src/pgRouter.js
+++ src/pgRouter.js
@@ -3,20 +3,20 @@ const db = require('./index')
 
 router.get('/', (_, response) => {
   db.query(
-    "SELECT * FROM users", [],
+    "SELECT * FROM quotes", [],
     (err, res) => {
-      if (err) response.send(err);
+      if (err) return response.send(err);
       response.status(200).json(res.rows)
     }
   )
 });
 
-router.get('/:name', (req, response) => {
+router.get('/:author', (req, response) => {
   db.query(
-    "SELECT name FROM users WHERE name = $1",
-    [req.params.name],
+    'SELECT * FROM quotes WHERE author_name = $1',
+    [req.params.author],
     (err, res) => {
-      if (err) response.send(err);
+      if (err) return response.send(err);
       response.status(200).json(res.rows)
     }
   )
@@ -24,22 +24,25 @@ router.get('/:name', (req, response) => {
 
 router.post('/', (req, response) => {
   db.query(
-    "INSERT INTO users (name) VALUES ($1) RETURNING *;",
-    [req.body.name],
+    `WITH etc AS (
+      INSERT INTO author (name) VALUES ($1) ON CONFLICT DO NOTHING)
+    INSERT INTO quotes (text, author_name) VALUES ($2, $1) RETURNING *`,
+    [req.body.author, req.body.quote],
     (err, res) => {
-      if (err) response.send(err);
+      if (err) return response.send(err);
       response.status(200).json(res.rows)
     }
   )
 });
 
-router.delete('/:name', (req, response) => {
+router.delete('/:id', (req, response) => {
+  console.log(req.params.id)
   db.query(
-    "DELETE FROM users WHERE name = $1",
-    [req.params.name],
+    "DELETE FROM quotes WHERE id = $1",
+    [req.params.id],
     (err, res) => {
-      if (err) response.send(err);
-      response.status(200).json(res.rows)
+      if (err) return response.send(err);
+      response.status(200).json({msg: "Quote deleted"});
     }
   )
 })
